87e7daca63a803a63eb823e51c18935d18c25d6c
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-04-27 22:52:29 +0530

    Time: 2 ms (95.26%), Space: 47.7 MB (39.36%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n= nums.length;
+        int [] freq=new int [n+1];
+        int [] ans= new int [2];
+
+        for(int i=0;i<n;i++ )
+          freq[nums[i]]++;
+
+          for(int i=0;i<n+1;i++)
+            if(freq[i]==2)  ans[0]=i;
+            else if(freq[i]==0)  ans[1]=i;
+            return ans;
+        
+    }
+}
