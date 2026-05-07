f775e56ec3190c1908e41fb0aa27ab935d3414da
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-05-07 23:10:36 +0530

    Time: 5 ms (87.1%), Space: 202.8 MB (12.9%) - LeetHub

--- 3660-jump-game-ix/3660-jump-game-ix.java
+++ 3660-jump-game-ix/3660-jump-game-ix.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] maxValue(int[] nums) {
-        int n= nums.length ,min=Integer.MIN_VALUE;
+        int n= nums.length ;
 
         int []max=new int [n];
         max[0]=nums[0];
@@ -9,7 +9,7 @@ class Solution {
             max[i]=Math.max(nums[i],max[i-1]);
         }
          
-         min=nums[n-1];
+         int min=nums[n-1];
         for(int i=n-2;i>=0;i--){
             min=Math.min(min,nums[i+1]);
             
