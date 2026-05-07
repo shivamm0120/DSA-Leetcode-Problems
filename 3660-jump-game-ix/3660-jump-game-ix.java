8e0631ab23a13eb1d7e6c8887bee1b1f4d9fd090
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-05-07 23:14:38 +0530

    Time: 4 ms (100%), Space: 202.9 MB (12.9%) - LeetHub

--- 3660-jump-game-ix/3660-jump-game-ix.java
+++ 3660-jump-game-ix/3660-jump-game-ix.java
@@ -5,19 +5,17 @@ class Solution {
         int []max=new int [n];
         max[0]=nums[0];
 
+        //find prefix max
         for(int i=1;i<n;i++){
             max[i]=Math.max(nums[i],max[i-1]);
         }
-         
+
+        
         for(int i=n-2;i>=0;i--){
             min=Math.min(min,nums[i+1]);
             
             if(max[i]>min)max[i]=max[i+1];
         }
-        
-
-        
       return max;  
-        
     }
 }
