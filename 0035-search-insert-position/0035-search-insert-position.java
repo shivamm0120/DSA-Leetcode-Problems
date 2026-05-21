a95726ebf597d058f0ce628769a1458a92a1526a
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-05-22 00:37:08 +0530

    Time: 0 ms (100%), Space: 44.5 MB (81.41%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -3,17 +3,8 @@ class Solution {
         int low=0;
         int high=nums.length-1;
 
-        int position=0;
         while(low<=high){
-                if(nums[low]>target){
-                    position=low;
-                    break;
-                }
-                if(nums[high]<target){
-                    position=high+1;
-                    break;
-                }
-                
+               
             int index=low+(high-low)/2;
             int mid_value=nums[index];
             if(mid_value==target)return index;
@@ -24,7 +15,7 @@ class Solution {
                 high=index-1;
             }
         }
-        return position;
+        return low;
         
     }
 }
