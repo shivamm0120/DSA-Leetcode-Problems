d4ba4585af91e9df5668a0e077a83cf585991319
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-05-24 15:47:50 +0530

    Time: 0 ms (100%), Space: 44 MB (86.12%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -12,9 +12,8 @@ class Solution {
             return mid;
             else if(nums[mid]>nums[mid-1])
                  low=mid+1;
-            else if(nums[mid]<nums[mid-1])
+            else
                  high=mid-1;
-            else low=mid+1;
         }
         return -1;
         
