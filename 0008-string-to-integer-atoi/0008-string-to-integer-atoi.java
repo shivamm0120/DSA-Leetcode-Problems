ffba6f3c9d43e723b5f50ffdb61d4036dd7e9ced
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-06-07 12:58:03 +0530

    Time: 1 ms (100%), Space: 43.9 MB (40.97%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -2,38 +2,33 @@ class Solution {
     public int myAtoi(String s) {
         
         int n = s.length();
-        int i = 0;
         int sign = 1;
         long num = 0;
-
-        // Skip leading spaces
-        while (i < n && s.charAt(i) == ' ')
-            i++;
-
-        // Handle sign
-        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
-            if (s.charAt(i) == '-')
-                sign = -1;
+        int i=0;
+        while(i<n&&s.charAt(i)==' '){
             i++;
         }
-
-        // Process digits
-        while (i < n && Character.isDigit(s.charAt(i))) {
-
-            int digit = s.charAt(i) - '0';
-            num = num * 10 + digit;
-
-            // Clamp if out of range
-            if (sign * num > Integer.MAX_VALUE)
-                return Integer.MAX_VALUE;
-
-            if (sign * num < Integer.MIN_VALUE)
-                return Integer.MIN_VALUE;
-
-            i++;
+        if(i<n && (s.charAt(i)=='+'||s.charAt(i)=='-')){
+            if(s.charAt(i)=='-')
+              sign=-1;
+              i++;
         }
+        return (int)convert(s,i,n,sign,num);
 
-        return (int)(sign*num);
         
+    }
+    static long convert(String s,int i,int n,int sign,long num){
+        if(i>=n || !Character.isDigit(s.charAt(i))){
+            return sign*num;
+        }
+        int digit=s.charAt(i)-'0';
+        num=num*10+digit;
+        if(sign*num>Integer.MAX_VALUE){
+          return Integer.MAX_VALUE;
+        }
+        if(sign*num<Integer.MIN_VALUE)
+        return Integer.MIN_VALUE;
+       
+        return convert(s,i+1,n,sign,num);
     }
 }
