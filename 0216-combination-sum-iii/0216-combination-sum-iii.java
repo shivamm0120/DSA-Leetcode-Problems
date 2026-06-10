3c98d9680b1177ce06cbd185639bfc4553e0af1e
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-06-10 09:37:10 +0530

    Time: 0 ms (100%), Space: 42.2 MB (90.79%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.java
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -6,17 +6,23 @@ class Solution {
          return ans;
         
     }
-    void findCombination(int start,int k,int n,List<List<Integer>>ans,List<Integer>al){
-        if(n==0){
-            if(al.size()==k)
-            ans.add(new ArrayList<>(al));
+    void findCombination(int start,int k,int target,List<List<Integer>>ans,List<Integer>curr){
+        if(target==0){
+            if(curr.size()==k)
+            ans.add(new ArrayList<>(curr));
          return; 
         }
-        if(start>n ||al.size()>=k)return;
+
+        //either target becomes -ve or when no of choices is k but the target is not 0  so we cant get further soln 
+        if(target<0 ||curr.size()>=k)return;
+
         for(int i=start;i<=9;i++) {
-            al.add(i);
-            findCombination(i+1,k,n-i,ans,al);
-            al.remove(al.size()-1);
+            //
+            if(i>target) break;
+
+            curr.add(i);
+            findCombination(i+1,k,target-i,ans,curr);
+            curr.remove(curr.size()-1);
         } 
     }
 }
