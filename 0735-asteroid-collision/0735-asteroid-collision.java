d20298eee32055c37655a0e405997af2fc46148a
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-07-03 12:56:33 +0530

    Time: 116 ms (5.38%), Space: 47 MB (57.72%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,30 +1,29 @@
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
         int n=asteroids.length;
-        int []size= new int [n];
-        for(int i=0;i<n;i++){
-            size[i]=Math.abs(asteroids[i]);
-        }
+       
         Deque<Integer>stack= new ArrayDeque<>();
 
         for(int i=0;i<n;i++){
 
             while(!stack.isEmpty()&& (stack.peek()>0 && asteroids[i]<0)){
-                if(stack.peek()>size[i]){
-                    size[i]=0;break;
+                int incomingSize=Math.abs(asteroids[i]);
+                if(stack.peek()>incomingSize){
+                    asteroids[i]=0;break;
                 }
-                else if(stack.peek()<size[i]){
+                else if(stack.peek()<incomingSize){
                     stack.pop();
                 }
                 else {
-                    size[i]=0;
+                    asteroids[i]=0;
                     stack.pop();
                 }
             }
-            if(size[i]!=0)stack.push(asteroids[i]);
+            if(asteroids[i]!=0)stack.push(asteroids[i]);
 
         }
-
+        
+        //create ans array that can be returned
        int []ans= new int[stack.size()];
        for(int i=ans.length-1;i>=0;i--){
         ans[i]=stack.pop();
