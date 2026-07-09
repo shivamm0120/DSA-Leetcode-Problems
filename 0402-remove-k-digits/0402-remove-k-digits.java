ed605f9ddd994b50ca73550edb7e6e53cd9304f0
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-07-09 13:15:03 +0530

    Time: 63 ms (10.61%), Space: 47.4 MB (56.97%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -1,30 +1,29 @@
 class Solution {
     public String removeKdigits(String num, int k) {
         StringBuilder ans=new StringBuilder();
-        char[] ch= num.toCharArray();
-        int n=ch.length;
-
         Deque<Character> stack= new ArrayDeque<>();
-        int i=0;
+        
+        //maintain a monotonic increasing stack such that if current ele is smaller than the top element then do pop 
+        for(char digit :num.toCharArray()){
 
-        while(i<n){
-
-            while(k>0 && !stack.isEmpty()&& stack.peek()>ch[i]){
+            while(k>0 && !stack.isEmpty()&& digit <stack.peek()){
                stack.pop();
                 k--;
             }
-            stack.push(ch[i]);
-            i++;
+            stack.push(digit);
         }
     
+    // if there is some more k then pop again 
         while(k>0 && !stack.isEmpty()){
             stack.pop();
             k--;
         }
 
+        //convert stack into its equivalent no 
         while(!stack.isEmpty()){
            ans.insert(0, stack.pop());
         }
+        //removing leading zeroes 
         while(ans.length()>0 && ans.charAt(0)=='0')ans.deleteCharAt(0);
         return ans.length()==0?"0":ans.toString();
         
