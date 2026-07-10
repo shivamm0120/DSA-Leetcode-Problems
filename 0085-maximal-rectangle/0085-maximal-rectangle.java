85a92d73448660d258e5204425688aaa1bf147a4
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-07-10 11:54:55 +0530

    Time: 15 ms (86.82%), Space: 47.6 MB (99.11%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -0,0 +1,78 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        int rows= matrix.length;
+        int cols= matrix[0].length;
+
+        int[] heights= new int [cols];
+        int maxArea=0;
+
+        for(int i=0;i<rows;i++){
+
+            for(int j=0;j<cols;j++){
+                if(matrix[i][j]=='1'){
+                    heights[j]++;
+                }
+                else heights[j]=0;
+            }
+            if(i==0)
+             for(int height: heights)
+               System.out.println(height);
+            
+            
+            maxArea= Math.max(maxArea, largestRectangleArea(heights));
+
+
+        }
+        return maxArea;
+
+
+        
+    }
+     public int largestRectangleArea(int[] heights) {
+
+        int [] prevSmaller=findPrevSmaller(heights);
+        int [] nextSmaller=findNextSmaller(heights);
+
+        int max=0;
+        for(int i=0;i<heights.length;i++){
+            int area= (nextSmaller[i]-prevSmaller[i]-1) *heights[i];
+
+            max=Math.max(max,area);
+        }
+        return max;
+
+        
+    }
+    int[] findPrevSmaller(int []heights){
+        int n=heights.length;
+        int []prev= new int[n];
+        Deque<Integer>stack= new ArrayDeque<>();
+
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && heights[i]<=heights[stack.peek()]){
+                stack.pop();
+            }
+           prev[i]= stack.isEmpty() ?-1:stack.peek();
+           stack.push(i);
+
+        }
+        return prev;
+
+    }
+    int[] findNextSmaller(int []heights){
+        int n=heights.length;
+        int []next= new int[n];
+        Deque<Integer>stack= new ArrayDeque<>();
+
+        for(int i=n-1;i>=0;i--){
+            while(!stack.isEmpty() && heights[i]<=heights[stack.peek()]){
+                stack.pop();
+            }
+            next[i]=stack.isEmpty()? n:stack.peek();
+            stack.push(i);
+
+        }
+        return next;
+
+    }
+}
