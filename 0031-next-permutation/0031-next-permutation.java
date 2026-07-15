b3d7a430e52f5db5f459fc69b5f96c8f1724a2f5
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-07-15 23:57:53 +0530

    Time: 3 ms (3.22%), Space: 45 MB (5.33%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -3,7 +3,7 @@ class Solution {
         int n=nums.length;
         int index=-1;
 
-
+        //find the longest common prefix =>means index from where i can get the next greater number(permutation)
         for(int i=n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
                 index=i;
@@ -11,8 +11,12 @@ class Solution {
                 break;
             }
         }
+        //when index is -1 ,it means that there is no any element at index i which is lesser than i+1 .In other words array is in decreasing order .So in this case we didnot find any index that can be changed by some bigger no.
+        //if we change any no by other no then we will get smaller value ..but it should be greater . Ex- [5,4,3,2]
+
         if (index==-1)
-          reverse(nums,0,n-1);
+          reverse(nums,0,n-1);//As ques demands give 1st smallest permutation in this case
+          
         else{
 
         for(int i=n-1;i>index;i--){
