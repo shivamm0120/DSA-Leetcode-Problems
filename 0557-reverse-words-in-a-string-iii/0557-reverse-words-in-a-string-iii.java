d1ec90162289dde5ab43ae758222b37ca089b6e1
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-07-23 21:21:25 +0530

    Time: 3 ms (99.58%), Space: 46.4 MB (89.24%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.java
@@ -1,38 +1,25 @@
 class Solution {
     public String reverseWords(String s) {
-       int n=s.length();
-       int start=0,end=0;
-       StringBuilder ans= new StringBuilder();
-
-       for( end=0;end<n;end++){
-            if(s.charAt(end)==' '){
-                String reversed =reverse(s.substring(start,end));
-                ans.append(reversed);
-                ans.append(' ');
-                start=end+1;
-            }
-       }
-       if(start<end){
-         String reversed =reverse(s.substring(start,end));
-         ans.append(reversed);
-
-       }
-      
-       return ans.toString();
+      char[] ch = s.toCharArray();
+        int n = ch.length;
+        int start = 0;
         
-    }
-    public String reverse (String s){
-        char[]ch= s.toCharArray();
-        int left=0;int right=ch.length-1;
-
-        while(left<right){
-            char temp=ch[left];
-            ch[left]=ch[right];
-            ch[right]=temp;
-
-            left++;right--;
-
+        for (int end = 0; end < n; end++) {
+            if (ch[end] == ' ') {
+                reverse(ch, start, end - 1);
+                start = end + 1;
+            }
         }
+        reverse(ch, start, n - 1);  
         return new String(ch);
+    }
+    private void reverse(char[] ch, int left, int right) {
+        while (left < right) {
+            char temp = ch[left];
+            ch[left] = ch[right];
+            ch[right] = temp;
+            left++;
+            right--;
+        }
     }
 }
