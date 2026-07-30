4be9cad69b7649f9867799cc5b3bca3ed0158c9f
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-07-30 18:07:34 +0530

    Time: 0 ms (100%), Space: 43.2 MB (66.51%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -2,7 +2,7 @@ class Solution {
     public int minimumPushes(String word) {
         int n =word.length();
 
-    //find how many cycles will be there if i map first char to the first digit,second to the 2nd ,3rd to the 3rd and so on . There is total 8 digits..so after mapping with the 8th digit ..start remapping from the first digit and keep going..
+    //find how many cycles will be there if i map first char to the first digit,second to the 2nd ,3rd to the 3rd and so on . There is total 8 digits..so after mapping with the 8th digit there will be an increment in cycles count ...now restart remapping from the first digit and keep going..
     //here i used formula for sum of 1st n natural no to count no of pushes if there  is some cycles .
         int cycles= n/8;
         int push =cycles*(cycles+1)/2 *8;
