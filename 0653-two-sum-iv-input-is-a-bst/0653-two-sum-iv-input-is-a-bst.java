d452fdf7cfdb92465dc7e4da7b8825c901308934
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-09-13 18:30:06 +0530

    Time: 7 ms (5.69%), Space: 50.2 MB (10.41%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -14,21 +14,45 @@
  * }
  */
 class Solution {
+    Stack<TreeNode>s1=new Stack<>();
+    Stack<TreeNode>s2=new Stack<>();
+
     public boolean findTarget(TreeNode root, int k) {
-        List<Integer>al=new ArrayList<>();
-        return findSum(al,root,k);
-
-
-
-        
+        push1(root);
+        push2(root);
+        return findTwoSum(k);
     }
-    boolean findSum(List<Integer>al,TreeNode root,int k){
-        if(root==null)return false;
+    boolean findTwoSum(int k){
 
-        int complement=k-root.val;
-        if(al.contains(complement))return true;
-        else al.add(root.val);
-        if(findSum(al,root.left,k)||findSum(al,root.right,k))return true;
-        return false;
+        if(s1.isEmpty()|| s2.isEmpty())return false;
+        if(s1.peek()==s2.peek())return false;
+
+        TreeNode node1=s1.peek();
+        TreeNode node2=s2.peek();
+
+        int sum=node1.val + node2.val;
+        if(sum==k)return true;
+        if(sum<k){
+           TreeNode node= s1.pop();
+           push1(node.right);
+        }
+        else{
+            push2(s2.pop().left);
+        }
+        return findTwoSum(k);
+
     }
+    public void push1(TreeNode node){
+        while(node!=null){
+            s1.push(node);
+            node=node.left;
+        }
+    }
+    public void push2(TreeNode node){
+        while(node!=null){
+            s2.push(node);
+            node=node.right;
+        }
+    }
+
 }
