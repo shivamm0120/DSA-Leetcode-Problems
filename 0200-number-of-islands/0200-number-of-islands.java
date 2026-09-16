fdf9bf9fcb30b8bb28e54623e25fdb6f532c3898
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-09-16 08:56:15 +0530

    Time: 4 ms (46.17%), Space: 51.8 MB (76.29%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -18,16 +18,18 @@ class Solution {
         
     }
     void dfs(int i,int j,char[][]grid,boolean[][] visited){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length ||grid[i][j]=='0' || visited[i][j])
+            return ;
+
+        //mark it as visited
         visited[i][j]=true;
 
-        if(j!=0 && !visited[i][j-1] && grid[i][j-1]=='1')
+        //go through each direction
         dfs(i,j-1,grid,visited);
-
-        if(j!=grid[0].length-1 && !visited[i][j+1] && grid[i][j+1]=='1')
         dfs(i,j+1,grid,visited);
+        dfs(i-1,j,grid,visited);
+        dfs(i+1,j,grid,visited);
 
-        if(i!=0 && !visited[i-1][j] && grid[i-1][j]=='1')dfs(i-1,j,grid,visited);
-        if(i!=grid.length-1 && !visited[i+1][j] && grid[i+1][j]=='1')dfs(i+1,j,grid,visited);
         return;
     }
 }
