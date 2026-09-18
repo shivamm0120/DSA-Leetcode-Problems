6cbfd80e2c870140bae097d3b8e001a64904f803
Shivam Kumar <kumar.shivam2005@outlook.com>
2026-09-18 09:29:09 +0530

    Time: 3 ms (99.84%), Space: 47.3 MB (21.2%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,11 +1,16 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<Integer>[] graph=new ArrayList[numCourses];
+
+        //0->not processed,not completed(unvisited)
+        //1->processed but not completed(visiting)
+        //2->completed(visited)
         int[] visited=new int[numCourses];
 
         for(int i = 0; i < numCourses; i++)
             graph[i] = new ArrayList<>();
-        
+
+        //create adjacency list
         for(int[] p :prerequisites){
             graph[p[0]].add(p[1]);
 
@@ -21,10 +26,15 @@ class Solution {
 
 
     boolean finishCourse(int i,List<Integer>[] graph,int[]visited){
+        //if u are visiting the same course again=>it means there is cycle.
+        //bcz if its prerequisites had completed then it should not be 1
+        //Means u r checking same course again 
          if(visited[i]==1)return false;
          visited[i]=1;
          
         for(int prerequisite:graph[i]){
+            //if already completed then dont go to complete it
+            //if not,then first finish the prerequisite:
             if(visited[prerequisite]!=2)
               if(!finishCourse(prerequisite,graph,visited))return false;
         }
